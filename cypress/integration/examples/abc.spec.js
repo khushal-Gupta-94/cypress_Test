07b837cec51b24b15126ec62cdfce82c3eded99a
Khushal Gupta <khushal.gupta@peak.ai>
2021-05-19 18:35:08 +0530

    test case added

---
+++ cypress/integration/examples/abc.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="Cypress" />
+describe('Run test', () => {
+    it('test 1', () => {
+        console.log('Test 1');
+    });
+    it('test 2', () => {
+        console.log('Test 2');
+    });
+});
---
+++ cypress/integration/examples1/xyz.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="Cypress" />
+describe('Run test demo', () => {
+    it('test 01', () => {
+        console.log('Test 1');
+    });
+    it('test 02', () => {
+        console.log('Test 2');
+    });
+});
