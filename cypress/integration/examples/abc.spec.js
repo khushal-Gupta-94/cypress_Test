f7ac4e2e484768276d98f0e62b160dcf9d9eb942
Khushal Gupta <khushal.gupta@peak.ai>
2021-05-21 19:21:22 +0530

    added test filter

--- cypress/integration/examples/abc.spec.js
+++ cypress/integration/examples/abc.spec.js
@@ -1,9 +1,14 @@
 /// <reference types="Cypress" />
+import filterTests from '../../support/filterTests';
 describe('Run test', () => {
+    filterTests(['pr','smoke'], () => {
     it('test 1', () => {
         console.log('Test 1');
     });
+});
+filterTests(['smoke'], () => {
     it('test 2', () => {
         console.log('Test 2');
     });
+});
 });
