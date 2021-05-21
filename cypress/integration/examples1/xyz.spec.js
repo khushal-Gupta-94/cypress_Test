7692a4ab6ed5940c094d21662015ca67561c7df3
Khushal Gupta <khushal.gupta@peak.ai>
2021-05-21 19:19:35 +0530

    added filter test

--- cypress/integration/examples1/xyz.spec.js
+++ cypress/integration/examples1/xyz.spec.js
@@ -1,9 +1,14 @@
 /// <reference types="Cypress" />
+import filterTests from '../../support/filterTests';
 describe('Run test demo', () => {
+    filterTests(['smoke'], () => {
     it('test 01', () => {
         console.log('Test 1');
     });
+});
+filterTests(['smoke'], () => {
     it('test 02', () => {
         console.log('Test 2');
     });
+});
 });
---
+++ cypress/support/filterTests.js
@@ -0,0 +1,15 @@
+/// <reference types="Cypress" />
+const filterTests = (definedTags, runTest) => {
+  if (Cypress.env('tags')) {
+    const tags = Cypress.env('tags').split('/');
+    const found = definedTags.some(($definedTag) => tags.includes($definedTag));
+
+    if (found) {
+      runTest();
+    }
+  } else {
+    runTest();
+  }
+};
+
+export default filterTests;
